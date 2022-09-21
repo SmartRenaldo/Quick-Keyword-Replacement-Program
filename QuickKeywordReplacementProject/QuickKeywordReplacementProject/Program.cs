19c97d581967f3200f1df1a52cc28ad3bc02ee35
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-21 16:08:43 +0930

    version 2 -- allow a keyword combined with more than one words

--- QuickKeywordReplacementProject/QuickKeywordReplacementProject/Program.cs
+++ QuickKeywordReplacementProject/QuickKeywordReplacementProject/Program.cs
@@ -10,6 +10,14 @@ namespace QuickKeywordReplacementProject
     {
         static void Main(string[] args)
         {
+            string[] oldKeySet = { "level" };
+            string[] newKeySet = { "device", "channel" };
+            string s = ReplaceKeyWord(oldKeySet, newKeySet,
+                "public"
+                );
+            Console.WriteLine(s);
+
+            Console.ReadKey();
         }
 
         static string ReplaceKeyWord(string oldKey, string newKey, string s)
@@ -27,6 +35,39 @@ namespace QuickKeywordReplacementProject
             return s.Replace(AllLowerOldKey, AllLowerNewKey)
                     .Replace(AllUpperOldKey, AllUpperNewKey)
                     .Replace(CamelOldKey, CamelNewKey);
+        }
+
+        static string ReplaceKeyWord(string[] oldKeySet, string[] newKeySet, string s)
+        {
+            //e.g. eeeee
+            StringBuilder AllLowerOldKeyBuilder = new StringBuilder();
+            //e.g. EEEEE
+            StringBuilder AllUpperOldKeyBuilder = new StringBuilder();
+            //e.g. EeeeeEeeee
+            StringBuilder CamelOldKeyBuilder = new StringBuilder();
+            StringBuilder AllLowerNewKeyBuilder = new StringBuilder();
+            StringBuilder AllUpperNewKeyBuilder = new StringBuilder();
+            StringBuilder CamelNewKeyBuilder = new StringBuilder();
+
+            foreach (string oldKey in oldKeySet)
+            {
+                AllLowerOldKeyBuilder.Append(oldKey.ToLower());
+                AllUpperOldKeyBuilder.Append(oldKey.ToUpper());
+                CamelOldKeyBuilder.Append(oldKey.Substring(0, 1).ToUpper());
+                CamelOldKeyBuilder.Append(oldKey.Substring(1).ToLower());
+            }
+
+            foreach (string newKey in newKeySet)
+            {
+                AllLowerNewKeyBuilder.Append(newKey.ToLower());
+                AllUpperNewKeyBuilder.Append(newKey.ToUpper());
+                CamelNewKeyBuilder.Append(newKey.Substring(0, 1).ToUpper());
+                CamelNewKeyBuilder.Append(newKey.Substring(1).ToLower());
+            }
+
+            return s.Replace(AllLowerOldKeyBuilder.ToString(), AllLowerNewKeyBuilder.ToString())
+                    .Replace(AllUpperOldKeyBuilder.ToString(), AllUpperNewKeyBuilder.ToString())
+                    .Replace(CamelOldKeyBuilder.ToString(), CamelNewKeyBuilder.ToString());
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 "# Quick-Keyword-Replacement-Program" 
 convert an old keyword's upper, lower, and camel cases into a new keyword's upper, lower, and camel cases
 
+Version 2: 
+    allow a keyword combined with more than one words
+    e.g. keyword "HelloWorld"
+
 Version 1:
     support a keyword with only one word
     e.g. keyword "Hello"
