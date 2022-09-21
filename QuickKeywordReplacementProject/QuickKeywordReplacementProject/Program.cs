4a74fe1e7a9ac722280cad071c2a8417da30428f
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-21 16:06:02 +0930

    version 1 -- support a keyword with only one word

---
+++ QuickKeywordReplacementProject/QuickKeywordReplacementProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKeywordReplacementProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static string ReplaceKeyWord(string oldKey, string newKey, string s)
+        {
+            //e.g. eeeee
+            string AllLowerOldKey = oldKey.ToLower();
+            //e.g. Eeeee
+            string CamelOldKey = oldKey.Substring(0, 1).ToUpper() + oldKey.Substring(1).ToLower();
+            //e.g. EEEEE
+            string AllUpperOldKey = oldKey.ToUpper();
+            string AllLowerNewKey = newKey.ToLower();
+            string CamelNewKey = newKey.Substring(0, 1).ToUpper() + newKey.Substring(1).ToLower();
+            string AllUpperNewKey = newKey.ToUpper();
+
+            return s.Replace(AllLowerOldKey, AllLowerNewKey)
+                    .Replace(AllUpperOldKey, AllUpperNewKey)
+                    .Replace(CamelOldKey, CamelNewKey);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 "# Quick-Keyword-Replacement-Program" 
+convert an old keyword's upper, lower, and camel cases into a new keyword's upper, lower, and camel cases
+
+Version 1:
+    support a keyword with only one word
+    e.g. keyword "Hello"
